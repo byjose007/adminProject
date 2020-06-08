07eeadf3c3398cf4e6b18310fccde2bfaf2e533f
Byron Armijos <byjose007@gmail.com>
2020-06-08 16:13:52 -0500

    Create project

--- admin-web/src/app/app-routing.module.ts
+++ admin-web/src/app/app-routing.module.ts
@@ -2,7 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard', 
+    pathMatch: 'full'
+  },
+  
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./modules/dashboard/dashboard.module').then( m => m.DashboardModule)
+  },
+  {
+    path: 'estadisticas',
+    loadChildren: () => import('./modules/estadisticas/estadisticas.module').then( m => m.EstadisticasModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ admin-web/src/app/modules/estadisticas/estadisticas-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { EstadisticasComponent } from './estadisticas.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: EstadisticasComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class EstadisticasRoutingModule { }
